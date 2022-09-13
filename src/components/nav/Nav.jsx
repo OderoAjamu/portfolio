7294fba2e23225568fe9e8f9b97316cf74a89cdc
Odero Ajamu <odero.ajamu@gmail.com>
2022-09-13 00:30:55 -0700

    Add a floating Nav bar with a hook to change state of selected section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {FiUser} from 'react-icons/fi'
+import {BiBookBookmark} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {TbMessage2} from 'react-icons/tb'
+import {useState} from 'react'
 
 export const Nav = () => {
+  const[activeNav,setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+    <a href="#" onClick={() => setActiveNav('#')}
+    className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+    <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FiUser/></a>
+    <a href="#experience" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiBookBookmark/></a>
+    <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+    <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><TbMessage2/></a>
+    </nav>
   )
 }
 export default Nav
